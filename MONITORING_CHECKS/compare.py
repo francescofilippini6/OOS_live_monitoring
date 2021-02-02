7dfc3b2e0d3883e6a88c74e6c7661582b1604525
francescofilippini6 <francesco.filippini3@gmail.com>
2021-02-02 17:23:14 +0000

    Images

--- MONITORING_CHECKS/compare.py
+++ MONITORING_CHECKS/compare.py
@@ -11,10 +11,30 @@ for i in doms_list:
     tmch_data = tmch_df[tmch_df['DOM']==i]
     srp_data = srp_df[srp_df['DOM']==i]
     fig=plt.figure()
+    fig.suptitle('DOM_'+str(i))
     for index,key in enumerate(tmch_df.keys()[-14:]):
         axx=fig.add_subplot(3,5,index+1)
         srp_data.plot(kind='scatter',x='time',y=key,color='red',ax=axx)
         tmch_data.plot(kind='scatter',x='time(s)',y=key,color='blue',ax=axx)
+        
+        axx.xaxis.set_major_locator(plt.MaxNLocator(2))
+        if index<10:
+            axx.tick_params(axis='x',which='both',bottom=False,top=False,labelbottom=False)
+            axx.set_xlabel(' ')
+
+        else:
+            axx.set_xlabel('time (s)',fontsize=6)
+            axx.tick_params(axis='x',labelsize=6)
+
+        axx.tick_params(axis='y',labelsize=6)           
+        axx.set_ylabel(str(key),fontsize=6)
+        axx.set_title(str(key), fontsize=10)
+        #axx.label_outer()
+        #axx.set_ylabel(" ")
+        
+        #if index != 13:
+        #    axx.set_xlabel(" ")
+        
     fig.tight_layout()
     fig.savefig('DOM_'+str(i)+'.png')
     
