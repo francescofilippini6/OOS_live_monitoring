310be6617d8e4ebef942f41cb1e95eff95b64963
francescofilippini6 <francesco.filippini3@gmail.com>
2021-02-02 14:14:12 +0000

    working programs...

--- MONITORING_CHECKS/TMCH_dumper.py
+++ MONITORING_CHECKS/TMCH_dumper.py
@@ -37,28 +37,28 @@ class OOSAnalyzer(Module):
         timestamp_converted = datetime.datetime.fromtimestamp(tmch_data.utc_seconds) 
         #print('local time: ',local_datetime_converted)
         timestamp=tmch_data.utc_seconds+tmch_data.nanoseconds*10**-9
-        print(timestamp)
+        #print(timestamp)
         #print('\nTimestamp = ',timestamp)
         #print('\nTimestamp in seconds = ',timestamp_converted)
         #print('DOM:',self.Dom_id_name[str(tmch_data.dom_id)])
         #print('Validity: ',tmch_data.flags)
         #print('Compass data: ',tmch_data.H)
-        self.testdf.loc[len(self.testdf)+1] = [self.Dom_id_name[str(tmch_data.dom_id)],timestamp_converted,timestamp,tmch_data.run,tmch_data.yaw,tmch_data.pitch,tmch_data.roll,tmch_data.A[0],tmch_data.A[1],tmch_data.A[2],tmch_data.G[0],tmch_data.G[1],tmch_data.G[2],tmch_data.H[0],tmch_data.H[1],tmch_data.H[2],tmch_data.temp,tmch_data.humidity]
-        
-        #sys.exit()
+        if tmch_data.nanoseconds*10**-9 == 0.5:
+            self.testdf.loc[len(self.testdf)+1] = [self.Dom_id_name[str(tmch_data.dom_id)],timestamp_converted,timestamp,tmch_data.run,tmch_data.yaw,tmch_data.pitch,tmch_data.roll,tmch_data.A[0],tmch_data.A[1],tmch_data.A[2],tmch_data.G[0],tmch_data.G[1],tmch_data.G[2],tmch_data.H[0],tmch_data.H[1],tmch_data.H[2],tmch_data.temp,tmch_data.humidity]
+            print("ciccio")
+            #sys.exit()
         
         
     def finish(self):
-        print(self.testdf.sort_values(['DOM','timeslice(s)']))
+        #print(self.testdf.sort_values(['DOM','timeslice(s)']))
+        self.testdf.sort_values(['DOM','timeslice(s)']).to_csv('12-min-TMCH_Data_parsed',index=0)
         print("-------BYE BYE!! (As catom (dialatt regian)-------)")
         
         
     
 def main():
     pipe=kp.Pipeline()
-    pipe.attach(kp.io.ch.CHPump , host='192.168.0.21', port=5553,tags='IO_MONIT',timeout=60 * 5,max_queue=2000)
-    #pipe.attach(kp.io.daq.TimesliceParser)    
-    #pipe.attach(kp.io.daq.DAQProcessor)
+    pipe.attach(kp.io.ch.CHPump , host='192.168.0.21', port=5553,tags='IO_MONIT',timeout=60*5,max_queue=2000)
     pipe.attach(OOSAnalyzer)
     pipe.drain()
 
---
+++ MONITORING_CHECKS/srp_parser.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+from collections import OrderedDict
+
+f = open("/home/km3net/analysis/MONITORING_CHECKS/Run12min","r")
+orderedDOM = [806451575,808981684,808447031,808985194,808971330,806451239,808952022,808967370,808489098,808976266,809537142,808984748,808982228,808980464,808976292,809544159,808996919]
+doms=list(range(1,18))
+ciccio=[]
+dom_list_upi = [] #list which contains all dom_i lists for all DOMs NOT USUED
+keys=[]
+lines=f.readlines()
+
+for index in range(len(lines)):
+    if "Data" in lines[index]:
+        for DOM in orderedDOM:
+            if str(DOM) in lines[index+1]:
+                upi=lines[index].split(" ")[1].split("d")
+                upifinal=upi[0][:-1]
+                dom_list_upi.append(upi[0][:-1])
+                ciccio.append(DOM)
+
+        
+#dic=OrderedDict(zip(orderedDOM,dom_list_upi))
+dic=OrderedDict(zip(ciccio,dom_list_upi))
+#for index in range(len(lines)):
+#    if "Data" in lines[index]:
+#        if dic[806451575] in lines[index]:
+#            newkey=lines[index].split("-")[3]
+#            keys.append(newkey)
+
+keys=['ahrs_pitch\n', 'ahrs_roll\n', 'ahrs_yaw\n', 'ahrs_a[0]\n', 'ahrs_a[1]\n', 'ahrs_a[2]\n', 'ahrs_g[0]\n', 'ahrs_g[1]\n', 'ahrs_g[2]\n', 'ahrs_h[0]\n', 'ahrs_h[1]\n', 'ahrs_h[2]\n', 'temp\n','humid\n']
+
+
+total_values=[]
+time_list=[]
+dom_list=[]
+#DOM_list=[]
+key_values=[]
+for key in keys:
+    
+    for index in range(len(lines)):
+        for dom in orderedDOM:
+            upi=dic[dom]
+            if upi in lines[index] and key in lines[index]:
+                ind = 0
+                while True:
+                    ind +=1
+                    if 'Data' in lines[index+ind]:
+                        break
+                    row = lines[index+ind]
+                    splitted_row = row.split(" ")
+                    time = splitted_row[0]+" "+splitted_row[1]+" "+splitted_row[2]
+                    value = splitted_row[3]
+                    if key == 'ahrs_pitch\n':
+                        time_list.append(time) 
+                        dom_list.append(dom)
+                    key_values.append(value)
+                    
+                total_values.append(key_values)
+                
+#print(dom_list[:10])
+#print(time_list[:10])
+#print(len(total_values[0]))               
+                    
+list_of_tuples = list(zip(dom_list,time_list,total_values[0],total_values[1],total_values[2],total_values[3],total_values[4],total_values[5],total_values[6],total_values[7],total_values[8],total_values[9],total_values[10],total_values[11],total_values[12],total_values[13]))            
+df = pd.DataFrame(list_of_tuples,columns=['DOM','time','yaw','pitch','roll','aX','aY','aZ','gX','gY','gZ','hX','hY','hZ','Temp','Humid'])
+
+df.to_csv("/home/km3net/analysis/MONITORING_CHECKS/datalog_parsed.csv",index=1)
+f.close()
